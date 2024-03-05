9de4d254e3682497260e3b071a844407c6f1f7c2
takumakoike <takuma.151e@gmail.com>
2024-03-05 20:41:43 +0900

    output

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "static",
+  // output: "static",
   // adapter: vercel()
 });
