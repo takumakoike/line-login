b5480353d7982b48450bb834b202af9b57354acc
takumakoike <takuma.151e@gmail.com>
2024-03-05 20:27:19 +0900

    adapter change

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 // import node from "@astrojs/node";
 
-import vercel from "@astrojs/vercel/serverless";
+// import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "static",
-  adapter: vercel()
+  // adapter: vercel()
 });
