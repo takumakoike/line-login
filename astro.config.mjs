5f69e5083c6313c705cede67e80a46c80f41dfb7
takumakoike <takuma.151e@gmail.com>
2024-03-05 20:43:06 +0900

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+// import tailwind from "@astrojs/tailwind";
 // import node from "@astrojs/node";
 
 // import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  // integrations: [tailwind()],
   // output: "static",
   // adapter: vercel()
 });
